ee97afc02f0482d9969b6f6c9188a67c22460195
deveraWIT <devera@wit.edu>
2024-01-30 15:52:13 -0500

    Added SceneBuilder

--- index.py
+++ index.py
@@ -20,19 +20,19 @@ def start_game():
     sheriff = Character("Sheriff")
     enemy = Character("Robber")
 
-    while sheriff.has_ended is not True:
-        user_input = input("Enter your command: ")
-        user_input_list = user_input.split()
-        if user_input_list[0] == "Move":
-            if sheriff.set_room(user_input_list[3]).room_id == user_input_list[3]:
-                new_room()
-        if user_input == "Inventory":
-            sheriff.display_items()
-        if user_input == "List items in Room":
-            sheriff.room.display_items()
-        if user_input_list[0] == "Pick" and user_input_list[1] == "up" and user_input_list[2] in sheriff.room.items:
-            sheriff.add_item(user_input_list[2])
-            sheriff.room.remove_item(user_input_list[2])
+    # while sheriff.has_ended is not True:
+    user_input = input("Enter your command: ")
+    user_input_list = user_input.split()
+    if user_input_list[0] == "Move":
+        if sheriff.set_room(user_input_list[3]).room_id == user_input_list[3]:
+            new_room()
+    if user_input == "Inventory":
+        sheriff.display_items()
+    if user_input == "List items in Room":
+        sheriff.room.display_items()
+    if user_input_list[0] == "Pick" and user_input_list[1] == "up" and user_input_list[2] in sheriff.room.items:
+        sheriff.add_item(user_input_list[2])
+        sheriff.room.remove_item(user_input_list[2])
 
     jail_room = Room("Jail")
     jail_room.add_item("Badge")
