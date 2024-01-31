19d42761765cdee9b9a9660ca8d7df21332d207f
deveraWIT <devera@wit.edu>
2024-01-31 02:25:49 -0500

    Cleaned things up a little and added some comments

--- item.py
+++ item.py
@@ -1,6 +1,10 @@
 from story import content
 import Room
 
+
+# NOTE: Items are currently being stored as strings, nothing more. This would provide more depth and allow for some cool
+# things if time permits
+
 class item:
     def __init__(self, name, location: Room):
         self.name = name
